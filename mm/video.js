89c5f107d65f1e290586a653802b0eb9b23b5e2e
tigerlivetv <118655573+tigerlivetv@users.noreply.github.com>
2024-07-24 17:13:34 +0630

    Create video.js

---
+++ mm/video.js
@@ -0,0 +1,21 @@
+// <![CDATA[
+jwplayer('reproductor').setup({
+//URL m3u8 que queremos reproducir
+file: "https://moodle1.playmudos.com/MU11QTgrRXRJdnBQMlB2WlJsZmxUa1BRMG0vQ0RCMEhFUUI5ZythR1oxOD0.m3u8",
+width: "700",
+skin: "five",
+height: "400",
+title: 'xsoundtv.com',
+image: "",
+logo: {
+file: "",
+position: 'top-right',
+margin: '0',
+link: ""
+},
+stretching: "fill",
+autostart: "true",
+abouttext: '',
+aboutlink: '',
+});
+// ]]>
